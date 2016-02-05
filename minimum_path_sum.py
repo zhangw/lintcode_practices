1cb8e4b736eb80b7b9c8b46e96d68e4261939f33
zhangw <jimokanghanchao@gmail.com>
2016-02-05 13:30:23 +0800

    使用循环替换了递归的实现，但循环的实现和递归一样，在数据量较大时，引发LintCode的内存使用限制异常

--- minimum_path_sum.py
+++ minimum_path_sum.py
@@ -11,6 +11,58 @@ minimum_path_sum.py
 Created by <jimokanghanchao@gmail.com> on Feb 04,2016
 """
 
+class Solution:
+    """
+    使用循环的方式替换递归实现对所有求和路径的遍历
+    此实现只返回路径和最小值，不保存求和路径
+    这个实现在数据量小的情况下(m*n <= 10 * 10)，可以运行。
+    数据量大时，LintCode的执行环境会抛出"Memory Limit Exceeded"的异常
+    """
+    
+    """
+    @param grid: a list of lists of integers.
+    @return: An integer, minimizes the sum of all numbers along its path
+    """
+    def minPathSum(self, grid):
+        nodes_with_current_sum = []
+        min_sum = None
+        current_sum = 0
+        while True:
+            if grid != None:
+                row = len(grid)
+                if row > 0:
+                    column = len(grid[0])
+                    if column > 0:
+                        if row > 1 and column > 1:
+                            #update the sum value
+                            current_sum += grid[0][0]
+                            #push the current sum and the downward node into stack
+                            nodes_with_current_sum.append((current_sum, grid[1:]))
+                            #firstly towards the right if it is possible
+                            grid = [row[1:] for row in grid]
+                            continue
+                        else:
+                            #just towards the right
+                            if row == 1:
+                                current_sum += sum(grid[0])
+                            #just downward
+                            if column == 1:
+                                current_sum += sum([row[0] for row in grid])
+                            #update the minimum sum value
+                            if min_sum == None:
+                                min_sum = current_sum
+                            else:
+                                min_sum = min(min_sum, current_sum)
+                            #recover the sum value and start the new path from the downward node
+                            if len(nodes_with_current_sum) > 0:
+                                current_sum, grid = nodes_with_current_sum.pop()
+                                continue
+                            else:
+                                #all paths have been visited, return the minimum sum
+                                return min_sum
+        return None
+
+
 class AnotherSolutionWithoutPaths:
     """
     只返回路径和最小值，不保存求和路径
@@ -110,10 +162,13 @@ def main():
     print "\n".join([str(row) for row in grid])
     solution = AnotherSolutionWithoutPaths()
     print "所有路径的最小和:%d" % solution.minPathSum(grid)
-    print "此实现不返回求和路径"
+    print "此递归实现不返回求和路径"
     solution = AnotherSolution()
     print "所有路径的最小和:%d" % solution.minPathSum(grid)
-    print "所有的求和路径:", solution
+    print "递归实现返回所有的求和路径:", solution
+    solution = Solution()
+    print "所有路径的最小和:%d" % solution.minPathSum(grid)
+    print "此非递归实现不返回求和路径"
 
 if __name__ == '__main__':
     main()
