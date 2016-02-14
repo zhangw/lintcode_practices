373fdee940975e4ba8485fc5e2978b0bcc49cc08
zhangw <jimokanghanchao@gmail.com>
2016-02-14 18:26:21 +0800

    使用递归实现深度遍历,并通过标零法标记被遍历过的岛屿,深度遍历在外循环被调用的次数就是岛屿的个数

--- number_of_islands.py
+++ number_of_islands.py
@@ -19,10 +19,17 @@ number_of_islands.py
 -----------------------------------------------------
 Created by <jimokanghanchao@gmail.com> on Feb 06,2016
 """
-class AnotherSolution:
+class Solution:
     """
-    此方法按行扫描的方式进行遍历，但是效率低下，会引起LintCode执行时间超出限制的异常
+    使用深度遍历和标零法，记录可以遍历的"1"的路径，遍历的次数就是岛屿的个数
+    参考:
+    https://segmentfault.com/a/1190000003753307
+    http://www.jiuzhang.com/problem/82/
     """
+    def init(self):
+        self.row = 0
+        self.column = 0
+
     # @param {boolean[][]} grid a boolean 2D matrix
     # @return {int} an integer
     def numIslands(self, grid):
@@ -32,58 +39,99 @@ class AnotherSolution:
                 column = len(grid[0])
                 if column > 0:
                     i = 0
-                    islands = []
-                    current_island = []
+                    counter_islands = 0
+                    self.row = row
+                    self.column = column
                     while i < row:
                         j = 0
                         while j < column:
-                            temp = grid[i][j]
-                            if temp == 1:
-                                current_island.append((i, j))
-                            else:
-                                if len(current_island):
-                                    islands.append(current_island)
-                                    current_island = []
-                                    self.mergeIslands(islands, i)
+                            start = grid[i][j]
+                            if start == 1:
+                                #DFS
+                                counter_islands += 1
+                                self.traverseIsland(grid, i, j)
                             j += 1
                         i += 1
-                        #IMPORTANT:initialize a new island object when before beginning of the new row
-                        if len(current_island):
-                            islands.append(current_island)
-                            current_island = []
-                            self.mergeIslands(islands, i)
-                    if len(current_island):
-                        islands.append(current_island)
-                        self.mergeIslands(islands, i)
-                    self.islands = islands
-                    return len(islands)
+                    return counter_islands
         return 0
 
-    def mergeIslands(self, islands, row_index):
-        if len(islands) > 1:
-            last_island = islands[-1]
-            for i in range(0, len(islands)-1):
-                    merged = self.mergeTwoIslands(islands[i], last_island)
-                    if merged:
-                        islands[i] = merged
-                        del islands[-1]
-                        return True
-        return False
-
-    def mergeTwoIslands(self, island, newisland):
-        for m in range(0, len(newisland)):
-            a = newisland[m]
-            if island.count((a[0]-1,a[1])) >= 1:
-                return island + newisland
-        return False
+    def traverseIsland(self, grid, x, y):
+        """
+        DFS travesal and make all the visited islands as 0
+        """
+        if grid[x][y] == 1:
+            grid[x][y] = 0
+            if x > 0 and grid[x-1][y] == 1:
+                #upward
+                self.traverseIsland(grid, x-1, y)
+            if x < self.row - 1 and grid[x+1][y] == 1:
+                #downward
+                self.traverseIsland(grid, x+1, y)
+            if y > 0 and grid[x][y-1] == 1:
+                #leftward
+                self.traverseIsland(grid, x, y-1)
+            if y < self.column - 1 and grid[x][y+1] == 1:
+                #rightward
+                self.traverseIsland(grid, x, y+1)
 
 def main():
-    matrix01 = [[1,0,0,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0],[1,0,0,1,1,0,0,1,0,0,0,1,0,1,0,1,0,0,1,0],[0,0,0,1,1,1,1,0,1,0,1,1,0,0,0,0,1,0,1,0],[0,0,0,1,1,0,0,1,0,0,0,1,1,1,0,0,1,0,0,1],[0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,0,1,0,1],[0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[0,0,0,1,0,1,0,0,1,1,0,1,0,1,1,0,1,1,1,0],[0,0,0,0,1,0,0,1,1,0,0,0,0,1,0,0,0,1,0,1],[0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,1,0],[1,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,1,0,1,0],[0,1,0,0,0,1,0,1,0,1,1,0,1,1,1,0,1,1,0,0],[1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1],[0,1,0,0,1,1,1,0,0,0,1,1,1,1,1,0,1,0,0,0],[0,0,1,1,1,0,0,0,1,1,0,0,0,1,0,1,0,0,0,0],[1,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,1,0,1,1],[1,0,1,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0],[0,1,1,0,0,0,1,1,1,0,1,0,1,0,1,1,1,1,0,0],[0,1,0,0,0,0,1,1,0,0,1,0,1,0,0,1,0,0,1,1],[0,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,0,0,0]]
+    """
+    matrix01 = [
+    [0,1,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1,0,1],
+    [1,0,1,0,0,1,1,0,0,1,0,1,0,1,0,1,1,0,0,0],
+    [0,1,0,0,0,1,1,1,1,0,0,0,0,0,1,1,1,1,0,1],
+    [1,1,0,0,0,1,1,0,0,0,1,1,1,0,0,1,0,1,1,0],
+    [0,1,0,1,1,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1],
+    [1,0,0,1,0,1,0,0,0,1,1,0,1,0,0,1,0,0,0,0],
+    [1,0,0,0,1,1,0,0,0,0,0,1,0,0,1,0,0,0,0,1],
+    [1,0,0,0,1,0,1,1,1,0,1,0,1,1,1,1,0,0,0,1],
+    [1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1],
+    [0,0,0,1,0,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0],
+    [1,0,1,0,1,0,0,1,1,1,0,1,1,0,0,1,1,0,0,0],
+    [0,1,0,0,1,0,0,0,0,0,0,1,1,1,1,0,0,0,1,0],
+    [1,0,0,0,1,1,1,0,1,0,0,0,1,0,1,0,1,0,0,1],
+    [0,0,0,0,1,0,1,1,0,1,0,1,0,1,1,1,1,0,0,0],
+    [0,1,1,0,0,0,0,1,0,0,1,1,1,0,0,1,1,0,1,0],
+    [1,0,1,1,1,1,1,1,0,1,1,0,1,0,0,1,0,0,0,1],
+    [1,0,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,1,1],
+    [0,0,1,0,0,0,0,1,0,0,1,1,0,1,1,1,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,1,1,0,1,0,1,0,0,0,1,1],
+    [1,0,0,0,1,0,1,1,1,0,0,1,0,1,0,1,1,0,0,0]
+    ]
+    """
+    """
+    matrix01 = [
+    [1,1,1,1,1,1],
+[1,0,0,0,0,1],
+[1,0,1,1,0,1],
+[1,0,0,0,0,1],
+[1,1,1,1,1,1]
+    ]
+    """
+    matrix01 = [
+ [0,1,1,1,1,0,0,1,0,1,1,1,1,1,1,0,0,1,1,0],
+ [0,0,0,0,0,0,1,0,1,0,1,0,0,1,1,0,0,0,1,0],
+ [0,1,0,1,0,1,0,1,0,0,1,1,1,0,0,0,0,0,0,1],
+ [1,0,0,1,1,1,1,1,0,1,0,0,0,0,1,0,1,0,1,0],
+ [0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1,1,1,0],
+ [0,0,0,0,0,0,0,1,0,1,1,0,0,1,1,0,0,1,0,1],
+ [0,0,1,1,1,1,0,1,1,1,1,0,0,0,1,0,1,0,1,1],
+ [1,1,1,1,0,0,1,0,1,0,0,0,0,0,0,1,1,1,0,1],
+ [1,0,0,0,1,1,0,0,1,0,1,1,0,0,0,1,0,0,0,0],
+ [1,0,0,1,0,1,0,1,0,0,1,1,1,0,0,0,0,0,1,0],
+ [0,0,0,1,0,0,1,0,0,0,0,0,0,1,0,1,0,0,1,1],
+ [0,1,0,0,1,1,1,0,1,1,1,0,0,0,1,0,1,0,0,0],
+ [1,1,1,0,1,1,0,1,0,1,0,1,1,0,0,0,1,0,0,0],
+ [0,1,1,0,1,0,0,1,1,0,1,1,0,1,1,0,1,1,0,0],
+ [0,1,1,0,0,1,0,0,0,1,0,1,0,0,0,0,0,0,1,1],
+ [0,0,1,0,0,0,1,1,0,1,1,1,1,0,1,0,1,0,1,0],
+ [1,0,0,0,1,0,1,0,1,0,0,0,0,1,1,0,0,0,0,0],
+ [1,0,0,1,1,0,0,1,1,0,0,0,1,0,0,0,1,1,1,0],
+ [0,1,0,1,1,0,0,0,1,1,0,0,1,1,1,0,0,1,1,0],
+ [0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,1,0]]
     print "01矩阵,0表示海洋,1表示岛屿:\n", "\n".join([str(row) for row in matrix01])
-    solution = AnotherSolution()
+    solution = Solution()
     print "岛屿的个数:%d" % solution.numIslands(matrix01)
-    islands = solution.islands
-    print "岛屿的组成:\n", "\n".join([str(island) for island in islands])
 
 if __name__ == '__main__':
     main()
